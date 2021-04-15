5f1016770223924e69fe5913a0cc5690daa3f77c
Weston Woodard <westonwoodard28@gmail.com>
2021-04-15 15:43:52 -0700

    data is coming in, but it is ugly.  Need to shape it better

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -1,12 +1,14 @@
 const db = require('../../data/dbConfig.js');
 
-function getRecipeById(recipe_id) {
-    return db('recipes')
+async function getRecipeById(recipe_id) {
+    const data = await db('recipes')
         .join('steps','steps.recipe_id','recipes.recipe_id')
         .join('ingredients_for_steps','ingredients_for_steps.step_id','steps.step_id')
         .join('ingredients',"ingredients.ingredient_id",'ingredients_for_steps.ingredient_id')
         .select('recipes.recipe_id as ID', 'recipes.recipe_name as Recipe','steps.step_id as stepID','steps.step_order as stepOrder','steps.step_instructions as Instructions','ingredients.ingredient_name as Ingredient')
-        .where('recipe_id',recipe_id).first()
+        .where('recipes.recipe_id',recipe_id)
+
+        const processedData = data
 }
 
 // getRecipeById(recipe_id)
@@ -17,3 +19,25 @@ function getRecipeById(recipe_id) {
 module.exports = {
 getRecipeById
 }
+
+// {
+//     "recipe_id" : 1,
+//     "recipe_name": "Spaghetti Bolognese",
+//     "created_at": "2021-01-01 08:23:19.120",
+//     "steps": [
+//       {
+//         "step_id": 11,
+//         "step_number": 1,
+//         "step_instructions": "Put a large saucepan on a medium heat",
+//         "ingredients": []
+//       },
+//       {
+//         "step_id": 12,
+//         "step_number": 2,
+//         "step_instructions": "Add 1 tbsp olive oil",
+//         "ingredients": [
+//           { "ingredient_id": 27, "ingredient_name": "olive oil", "quantity": 0.014 }
+//         ]
+//       },
+//     ]
+//   }
